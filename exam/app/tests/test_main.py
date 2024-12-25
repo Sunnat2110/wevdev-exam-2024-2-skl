e23b0604bf85ab28292b71a1f0115896f27b2c6e
Sunnat <sunnatk04@icloud.com>
2024-12-25 19:48:49 +0300

    rm files

--- exam/app/tests/test_main.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from app.main import process_numbers
-
-def test_process_numbers():
-	assert process_numbers() == "Result = 12", "Result must be 'Result = 12'"
--- exam/app/tests/test_utils.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from app.utils import add
-
-def test_add():
-	assert add(2, 3) == 5, "2 + 3 = 5"
-	assert add(-1, 1) == 0, "-1 + 1 = 0"
