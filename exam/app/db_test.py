145a91ca05322a625b572c4e6cde50ab392c9618
Sunnat2110 <sunnatk04@icloud.com>
2024-12-26 22:28:37 +0300

    tests

--- exam/app/db_test.py
+++ exam/app/db_test.py
@@ -5,4 +5,4 @@ def test_add():
 	assert add(2, 3) == 5, "db is reachable, select test is over"
 
 def test_add1():
-	assert add1(2, 3) == 1, "user1 is in the db, authorization test is over"
+	assert add1(2, 3) == 1, "user2 is not in the db, authorization test is over unseccessful"
